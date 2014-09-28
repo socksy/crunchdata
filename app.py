6bc1ce3711d767c83fd5f59c392e557b4a5876b7
Ben Lovell <ben.j.lovell@gmail.com>
2014-09-28 03:36:30 +0100

    basic API access

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ import sys
 
 base_url = "http://api.crunchbase.com/v/2/"
 try:
-    key = os.environ["CRUNCHKEY"]
+    user_key = os.environ["CRUNCHKEY"]
 except KeyError:
     print("Please export CRUNCHKEY as your crunchbase api_key (have you"    
             + " sourced ./secrets.sh ?)")
     sys.exit(1)
 
-payload = { "user_key": key}
-r = requests.get(base_url +'organizations', params=payload)
-print(r.text)
+def new_payload(**contents):
+    base_payload = { "user_key": user_key}
+    base_payload.update(contents)
+    return base_payload
+
+def get_endpoint(endpoint, pages=1, **payload):
+    results = []
+    for i in range(pages):
+        results.append(get_endpoint_page(endpoint, i, payload))
+    return results
+
+def get_endpoint_page(endpoint, page, payload):
+    payload = new_payload(page=str(page), **payload)
+    r = requests.get(base_url + endpoint, params=payload)
+    return r.json()['data']['items']
+
+if __name__ == "__main__":
+    companies = get_endpoint('organizations', 1, organization_types="company")
+    print(companies)

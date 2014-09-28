92bf364abf72ee052caaf8bf99e1d541ef711399
Ben Lovell <ben.j.lovell@gmail.com>
2014-09-28 02:42:32 +0100

    connect to API

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 import json
 import requests
+import os
+import sys
+
+base_url = "http://api.crunchbase.com/v/2/"
+try:
+    key = os.environ["CRUNCHKEY"]
+except KeyError:
+    print("Please export CRUNCHKEY as your crunchbase api_key (have you"    
+            + " sourced ./secrets.sh ?)")
+    sys.exit(1)
+
+payload = { "user_key": key}
+r = requests.get(base_url +'organizations', params=payload)
+print(r.text)
